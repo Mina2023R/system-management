bee15cd7e2f7129d77d33fedfc335797c6dd3e0d
Mina2023R <130800496+Mina2023R@users.noreply.github.com>
2023-04-30 18:33:16 +0200

    update ,delete item and delete all

--- shop.js
+++ shop.js
@@ -9,6 +9,10 @@ let count=document.getElementById('count');
 let category=document.getElementById('category');
 let sub=document.getElementById('sub');
 
+let mood='create';
+let tmp;
+
+
 //1-get total
 function gettotal(){
     //You must make sure that the price is not empty
@@ -51,7 +55,20 @@ function gettotal(){
                 category:category.value,
               }
   //console.log(newpro)   //add object in array
-  datapro.push(newpro)
+  if(mood==='create'){if(newpro.count>1){
+    for(let i=0;i<newpro.count;i++){
+        datapro.push(newpro)  
+    }
+  }else{
+  datapro.push(newpro)}
+
+  }else{
+    datapro[tmp]=newpro;
+    mood='create';
+    sub.innerHTML=`create`;
+    count.style.display='block';
+  }
+  
   //save date in localstorage
   //use json to convert to string   
   localStorage.setItem(`goter`,JSON.stringify(datapro))
@@ -89,7 +106,7 @@ function gettotal(){
                     <td>${datapro[i].profit}</td>
                     <td>${datapro[i].total}</td>
                     <td>${datapro[i].category}</td>
-                    <td><button>update</button></td>
+                    <td><button onclick="update(${i})">update</button></td>
                     <td><button onclick="Delet(${i})">Delete</button></td>
                 </tr>
             `
@@ -98,7 +115,7 @@ function gettotal(){
         document.getElementById("tbody").innerHTML=table
         let btndel=document.getElementById("btndel");
         if(datapro.length>0){
-            btndel .innerHTML=`<button onclick="del()">Delete All</button>`}
+            btndel .innerHTML=`<button onclick="del()">Delete All(${datapro.length}) </button>`}
             else{
                 btndel.innerHTML='';}
 
@@ -116,6 +133,87 @@ function gettotal(){
     function del(){
         datapro.splice(0)
         showdata()
+    }
+    //update
+    function update(i){
+        //console.log(i)
+    title.value=datapro[i].title;
+    price.value=datapro[i].price;
+    ads.value=datapro[i].ads;
+    taxes.value=datapro[i].taxes;
+    profit.value=datapro[i].profit;
+    gettotal()
+    count.style.display='none';
+    category.value=datapro[i].category;
+    sub.innerHTML=`update`;
+    mood='update';
+    tmp=i;
+    scroll({
+        top:0,
+        behavior:"smooth" 
+    })
+    }
+
+    //search
+    let searchmood='title';
+
+    function getsearchmood(id){
+        let search=document.getElementById('search')
+        if(id=='searchtitle'){
+            searchmood='title'
+            search.placeholder='search by title'
+        }else{
+            searchmood='category'
+            search.placeholder='search by category'
+
+        }
+      search.focus()
+      search.value='';
+      showdata()
+    }
+    function ser(value){
+        let table=``;
+        if(searchmood=='title'){for(i=0;i<datapro.length;i++){
+            
+                if(datapro[i].title.includes(value)){
+                    table +=`
+                    <tr>
+                    <td>${i}</td>
+                    <td>${datapro[i].title}</td>
+                    <td>${datapro[i].price}</td>
+                    <td>${datapro[i].ads}</td>
+                    <td>${datapro[i].taxes}</td>
+                    <td>${datapro[i].profit}</td>
+                    <td>${datapro[i].total}</td>
+                    <td>${datapro[i].category}</td>
+                    <td><button onclick="update(${i})">update</button></td>
+                    <td><button onclick="Delet(${i})">Delete</button></td>
+                </tr>
+            `
+                }
+            }
+        }else{
+            for(i=0;i<datapro.length;i++){
+            if(datapro[i].category.includes(value)){
+                table +=`
+                <tr>
+                <td>${i}</td>
+                <td>${datapro[i].title}</td>
+                <td>${datapro[i].price}</td>
+                <td>${datapro[i].ads}</td>
+                <td>${datapro[i].taxes}</td>
+                <td>${datapro[i].profit}</td>
+                <td>${datapro[i].total}</td>
+                <td>${datapro[i].category}</td>
+                <td><button onclick="update(${i})">update</button></td>
+                <td><button onclick="Delet(${i})">Delete</button></td>
+            </tr>
+        `
+            }
+        }
+
+        }
+        document.getElementById("tbody").innerHTML=table
 
     }
 
